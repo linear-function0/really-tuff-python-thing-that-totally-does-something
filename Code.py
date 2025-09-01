3efcaffc4330a2b11ee7d8b9c0dc71bc5737b676
Shadow <o4002184@gmail.com>
2025-08-31 23:48:45 -0400

    Created example code for linear-function0

---
+++ Code.py
@@ -0,0 +1,17 @@
+#just a random comment by maroon-shadow
+import time
+import os
+
+x = "Hi linear!"
+
+os.system("title Linear's code.py")
+
+print(x)
+
+time.sleep(2)
+
+print("\nIt took 2 seconds for this to be sent.")
+
+time.sleep(2)
+
+os.system("clr")
